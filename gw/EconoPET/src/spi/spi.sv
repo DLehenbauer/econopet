03b94d1cd4bc4a3dcf9f9abba26adecd3cd926be
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2023-11-26 06:24:28 -0800

    gw(spi): set 'cycle_o' on preceding falling SCK edge

--- gw/EconoPET/src/spi/spi.sv
+++ gw/EconoPET/src/spi/spi.sv
@@ -28,19 +28,17 @@
 
     output logic [DATA_WIDTH - 1:0] data_o,     // Data received.  Shifted in from SDO on each positive edge of SCK.
     
-    output logic                    cycle_o     // Asserted when 'data_o' is valid (and next 'data_i' is required).
+    output logic                    cycle_o     // Asserted on rising SCK when 'data_o' is valid.  The next 'data_i'
+                                                // is captured on the following negative SCK edege.
 );
     logic [$clog2(DATA_WIDTH - 1) - 1:0] bit_count = '0;
     
     initial begin
+        cycle_o   <= '0;
         bit_count <= '0;
     end
 
-    // 'cycle_o' and 'data_o' must be valid on the rising edge of 'spi_sck_i'.  This requires combinitorial
-    // logic.
-
-    assign cycle_o = bit_count == DATA_WIDTH - 1;
-
+    // 'data_o' is latched on the rising edge of SCK.
     logic [DATA_WIDTH - 2:0] dout_q;
 
     always_latch begin
@@ -49,14 +47,14 @@
 
     always_ff @(posedge spi_cs_ni or posedge spi_sck_i) begin
         if (spi_cs_ni) begin
-            // Deasserting CS_N indicates the transfer has ended.
+            // Deasserting 'spi_cs_ni' indicates the transfer has ended.
             bit_count <= '0;
             dout_q <= 'x;
         end else begin
             // Positive edge of SCK indicates SDI is valid.  Shift SDI into the LSB of 'data_o' and
             // increment the bit count.  If this was the last bit of the current cycle assert 'cycle_o'.
             bit_count <= bit_count + 1'b1;
-            dout_q <= data_o[DATA_WIDTH - 2:0];
+            dout_q <= { dout_q[DATA_WIDTH - 3:0], spi_sd_i };
         end
     end
 
@@ -65,9 +63,8 @@
 
     always_ff @(negedge spi_cs_ni or negedge spi_sck_i) begin
         if (spi_sck_i) begin
-            // Negative edge of CS_N indicates a new tranfer is beginning.  Initialize 'tx_buffer'
-            // with contents of 'data_i'.
             tx_buffer <= data_i;
+            cycle_o <= '0;
         end else begin
             // Negative edge of SCK indicates we've finished transmitting a bit.  Load the next bit to transmit.
             if (bit_count == '0) begin
@@ -77,6 +74,10 @@
                 // Otherwise shift out the MSB from 'tx_buffer'.
                 tx_buffer <= { tx_buffer[DATA_WIDTH - 2:0], 1'bx };
             end
-        end
+
+            // When the final bit of 'data_i' has been shifted into 'spi_sd_o', the we know the following positive
+            // SCK transfers edge will transfer the final bits of 'data_i' and 'data_o'.
+            cycle_o <= bit_count == DATA_WIDTH - 1;
+        end;
     end
 endmodule
