90bc06ab5b1ce933ea08d11415ffaeb8a7e04c95
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2023-12-08 08:53:42 -0800

    fw: document GPIO pins in hw.h

--- fw/hw.h
+++ fw/hw.h
@@ -12,13 +12,36 @@
  * @author Daniel Lehenbauer <DLehenbauer@users.noreply.github.com> and contributors
  */
 
+#pragma region // SPI
+
+// SPI speeds faster than 24 MHz require overclocking 'peri_clk' or using PIO for SPI.
+// (See: https://github.com/Bodmer/TFT_eSPI/discussions/2432)
+#define SPI_MHZ 24
+
 #define SPI_INSTANCE spi1
+
+// SCK, SDO, and SDI are standard SPI bus signals driven by the PrimeCell SSP
 #define SPI_SCK_GP 14
 #define SPI_SDO_GP 11
 #define SPI_SDI_GP 12
-#define SPI_CS_GP 13
-#define SPI_STALL_GP 10
-#define SPI_MHZ 24
 
+// The CS output is controlled via software to avoid de-asserting CS after each byte
+// is transmitted, which would asynchronously reset the FPGA's SPI state machine.
+#define SPI_CS_GP 13
+
+// When the MCU asserts CS (signalling the beginning of a new command), the FPGA asserts
+// STALL until the full command has been received and processed.
+#define SPI_STALL_GP 10
+
+#pragma endregion
+
+#pragma region // FPGA
+
+// Pulsing CRESET initiates FPGA configuration.  After the CRESET signal is de-asserted,
+// the MCU uploads the FPGA binary via SPI0.
 #define FPGA_CRESET_GP 26
+
+// PWM output used to generate the PLL input for FPGA.
 #define FPGA_CLK_GP 15
+
+#pragma endregion
