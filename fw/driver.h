e87d90812fc126ae967f5d258dff6419f67c18d1
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2024-07-13 16:41:54 +0000

    fw: fix a few GCC warnings

--- fw/driver.h
+++ fw/driver.h
@@ -23,6 +23,6 @@ void spi_read_seek(uint32_t addr);
 uint8_t spi_read_at(uint32_t addr);
 uint8_t spi_read_next();
 
-void spi_write(uint32_t addr, uint32_t byteLength, const uint8_t const* pSrc);
+void spi_write(uint32_t addr, uint32_t byteLength, const uint8_t* const pSrc);
 void spi_write_at(uint32_t addr, uint8_t data);
 void spi_write_next(uint8_t data);
