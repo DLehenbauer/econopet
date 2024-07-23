204bc989467fc86c0eafcd23b3d7f44b58a70966
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2024-07-23 15:40:27 +0000

    gw: add 'load_rom()' to mock_ram

--- gw/EconoPET/sim/mock_ram.sv
+++ gw/EconoPET/sim/mock_ram.sv
@@ -13,8 +13,8 @@
  */
 
 module mock_ram #(
-    parameter integer unsigned DATA_WIDTH = 8,
-    parameter integer unsigned ADDR_WIDTH = 17
+    parameter integer unsigned ADDR_WIDTH = 17,
+    parameter integer unsigned DATA_WIDTH = 8
 ) (
     input  logic [ADDR_WIDTH-1:0] ram_addr_i,
     input  logic [DATA_WIDTH-1:0] ram_data_i,
@@ -23,6 +23,26 @@ module mock_ram #(
     input  logic                  ram_oe_n_i
 );
     logic [DATA_WIDTH - 1:0] mem[(2 ** ADDR_WIDTH) - 1];
+
+    task automatic load_rom(
+        input bit [ADDR_WIDTH-1:0] address,
+        input string filename
+    );
+        int file, status;
+
+        file = $fopen({ "../../build/roms/bin/roms/", filename }, "rb");
+        if (file == 0) begin
+            $fatal(1, "Error opening file '%s'.  Did you forget to build '/roms'?", filename);
+        end
+
+        status = $fread(mem, file, 32'(address));
+        if (status < 1) begin
+            $fatal(1, "Error reading file '%s' (status=%0d).", filename, status);
+        end
+
+        $display("[%t] Loaded ROM '%s' ($%x-%x, %0d bytes)", $time, filename, address, address + ADDR_WIDTH'(status - 1), status);
+        $fclose(file);
+    endtask
 
     always @(negedge ram_we_n_i or negedge ram_oe_n_i) begin
         if (!ram_we_n_i) begin
