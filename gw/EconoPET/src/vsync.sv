82099d8150d99515922881360fcaa0116f723356
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2024-07-25 19:01:13 +0000

    gw: update vsync hack

--- gw/EconoPET/src/vsync.sv
+++ gw/EconoPET/src/vsync.sv
@@ -5,13 +5,11 @@ module vsync(
 );
     logic [14:0] count = 0;
 
-    initial begin
-        v_sync_o = 1'b0;
-    end
+    assign v_sync_o = count[14];
 
     always_ff @(posedge cpu_clock_i) begin
-        if (count == 8333) begin
-            v_sync_o <= ~v_sync_o;
+        if (count == 16667) begin
+            count <= 0;
         end else begin
             count <= count + 1'd1;
         end
