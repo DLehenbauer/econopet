fce915fb96db8c40c01533aeae87921be75f4143
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2024-08-24 16:47:15 +0000

    gw: merge system.sv into main.sv

--- gw/EconoPET/src/main.sv
+++ gw/EconoPET/src/main.sv
@@ -67,9 +67,22 @@ module main #(
     output logic spi1_sd_o,   // (SDO) Serial Data Out (FPGA -> MCU)
     output logic spi_stall_o
 );
+    logic [WB_ADDR_WIDTH-1:0] wb_addr;
+    logic [   DATA_WIDTH-1:0] wb_dout;
+    logic [   DATA_WIDTH-1:0] wb_din;
+    logic                     wb_we;
+    logic                     wb_cycle;
+    logic                     wb_strobe;
+    logic                     wb_stall;
+    logic                     wb_ack;
+
+    //
+    // SPI <-> Wishbone Bridge
+    //
+
     logic [WB_ADDR_WIDTH-1:0] spi1_addr;
-    logic [   DATA_WIDTH-1:0] spi1_data_rx;
-    logic [   DATA_WIDTH-1:0] spi1_data_tx;
+    logic [   DATA_WIDTH-1:0] spi1_dout;
+    logic [   DATA_WIDTH-1:0] spi1_din;
     logic                     spi1_we;
     logic                     spi1_cycle;
     logic                     spi1_strobe;
@@ -79,8 +92,8 @@ module main #(
     spi1_controller spi1 (
         .wb_clock_i(sys_clock_i),
         .wb_addr_o(spi1_addr),
-        .wb_data_o(spi1_data_rx),
-        .wb_data_i(spi1_data_tx),
+        .wb_data_o(spi1_dout),
+        .wb_data_i(spi1_din),
         .wb_we_o(spi1_we),
         .wb_cycle_o(spi1_cycle),
         .wb_strobe_o(spi1_strobe),
@@ -95,52 +108,141 @@ module main #(
         .spi_stall_o(spi_stall_o)
     );
 
-    system system (
-        // Wishbone
-        .wb_clock_i(sys_clock_i),
-        .wb_addr_i(spi1_addr),
-        .wb_data_i(spi1_data_rx),
-        .wb_data_o(spi1_data_tx),
-        .wb_we_i(spi1_we),
-        .wb_cycle_i(spi1_cycle),
-        .wb_strobe_i(spi1_strobe),
-        .wb_stall_o(spi1_stall),
-        .wb_ack_o(spi1_ack),
+    // For now, SPI1 is the only controller on the Wishbone bus.
+    assign wb_addr      = spi1_addr;
+    assign wb_din       = spi1_dout;
+    assign spi1_din     = wb_dout;
+    assign wb_we        = spi1_we;
+    assign wb_cycle     = spi1_cycle;
+    assign wb_strobe    = spi1_strobe;
+    assign spi1_ack     = wb_ack;
+    assign spi1_stall   = wb_stall;
 
-        // CPU
-        .cpu_reset_i(cpu_reset_i),
-        .cpu_reset_o(cpu_reset_o),
-        .cpu_be_o(cpu_be_o),
-        .cpu_ready_o(cpu_ready_o),
-        .cpu_clock_o(cpu_clock_o),
-        .cpu_irq_i(cpu_irq_i),
-        .cpu_irq_o(cpu_irq_o),
-        .cpu_nmi_i(cpu_nmi_i),
-        .cpu_nmi_o(cpu_nmi_o),
-        .cpu_addr_i(cpu_addr_i),
-        .cpu_addr_o(cpu_addr_o),
-        .cpu_addr_oe(cpu_addr_oe),
-        .cpu_data_i(cpu_data_i),
-        .cpu_data_o(cpu_data_o),
-        .cpu_data_oe(cpu_data_oe),        
-        .cpu_we_i(cpu_we_i),
-        .cpu_we_o(cpu_we_o),
-        .cpu_we_oe(cpu_we_oe),
-        
-        // RAM
-        .ram_addr_a10_o(ram_addr_a10_o),
-        .ram_addr_a11_o(ram_addr_a11_o),
-        .ram_addr_a15_o(ram_addr_a15_o),
-        .ram_addr_a16_o(ram_addr_a16_o),
-        .ram_oe_o(ram_oe_o),
-        .ram_we_o(ram_we_o),
+    // For now, outgoing CPU control signals are constant.
+    assign cpu_ready_o = 1;
+    assign cpu_reset_o = 0;
+    assign cpu_irq_o   = 0;
+    assign cpu_nmi_o   = 0;
 
-        // IO
-        .io_oe_o(io_oe_o),
-        .pia1_cs_o(pia1_cs_o),
-        .pia2_cs_o(pia2_cs_o),
-        .via_cs_o(via_cs_o)
+    // For now, CPU always drives RWB, which is independent from RAM OE/WE.
+    assign cpu_we_o    = 0;
+    assign cpu_we_oe   = 0;
+
+    logic cpu_grant;
+    logic wb_grant;
+
+    timing timing (
+        .clock_i(sys_clock_i),
+        .cpu_grant_o(cpu_grant),
+        .wb_grant_o(wb_grant)
     );
+
+    cpu cpu (
+        .sys_clock_i(sys_clock_i),
+        .cpu_grant_i(cpu_grant),
+        .cpu_be_o(cpu_be_o),
+        .cpu_clock_o(cpu_clock_o)
+    );
+
+    //
+    // Wishbone <-> RAM Bridge
+    //
+
+    logic [RAM_ADDR_WIDTH-1:0] ram_wb_addr;
+    logic [    DATA_WIDTH-1:0] ram_wb_din;
+    logic [    DATA_WIDTH-1:0] ram_wb_dout;
+    logic                      ram_wb_we;
+    logic                      ram_wb_cycle;
+    logic                      ram_wb_strobe;
+    logic                      ram_wb_stall;
+    logic                      ram_wb_ack;
+
+    assign ram_wb_addr   = wb_addr[RAM_ADDR_WIDTH-1:0];
+    assign ram_wb_din    = wb_din;
+    assign wb_dout       = ram_wb_dout;
+    assign ram_wb_we     = wb_we;
+    assign ram_wb_cycle  = wb_cycle;
+    assign ram_wb_strobe = wb_grant & wb_strobe;
+    assign wb_stall      = ~wb_grant | ram_wb_stall;
+    assign wb_ack        = ram_wb_ack;
+
+    logic [RAM_ADDR_WIDTH-1:0] ram_ctl_addr;    // Captured address for read/write cycle
+    logic                      ram_ctl_oe;      // OE signal for read cycle
+    logic                      ram_ctl_we;      // WE signal for write cycle
+
+    ram ram (
+        .wb_clock_i(sys_clock_i),
+        .wb_addr_i(ram_wb_addr),
+        .wb_data_i(ram_wb_din),
+        .wb_data_o(ram_wb_dout),
+        .wb_we_i(ram_wb_we),
+        .wb_cycle_i(ram_wb_cycle),
+        .wb_strobe_i(ram_wb_strobe),
+        .wb_stall_o(ram_wb_stall),
+        .wb_ack_o(ram_wb_ack),
+
+        .ram_oe_o(ram_ctl_oe),
+        .ram_we_o(ram_ctl_we),
+        .ram_addr_o(ram_ctl_addr),
+        .ram_data_i(cpu_data_i),
+        .ram_data_o(cpu_data_o),
+        .ram_data_oe(cpu_data_oe)
+    );
+
+    //
+    // Address Decoding
+    //
+
+    logic ram_en;
+    logic pia1_en;
+    logic pia2_en;
+    logic via_en;
+    logic io_en;
+
+    address_decoding address_decoding(
+        .addr_i(cpu_addr_i),
+        .ram_en_o(ram_en),
+        .pia1_en_o(pia1_en),
+        .pia2_en_o(pia2_en),
+        .via_en_o(via_en),
+        .io_en_o(io_en),
+
+        // Not yet used
+        .sid_en_o(),
+        .magic_en_o(),
+        .crtc_en_o(),
+        .is_mirrored_o(),
+        .is_readonly_o()
+    );
+
+    wire cpu_rd_strobe = cpu_be_o && !cpu_we_i;
+    wire cpu_wr_strobe = cpu_be_o &&  cpu_we_i && cpu_clock_o;
+
+    assign io_oe_o   = io_en   && cpu_be_o;
+    assign pia1_cs_o = pia1_en && cpu_be_o;
+    assign pia2_cs_o = pia2_en && cpu_be_o;
+    assign via_cs_o  =  via_en && cpu_be_o;
+
+    assign ram_oe_o         = (cpu_rd_strobe && ram_en) || ram_ctl_oe;
+    assign ram_we_o         = (cpu_wr_strobe && ram_en) || ram_ctl_we;
+
+    assign cpu_addr_oe      = !cpu_be_o;
+    assign cpu_addr_o       = ram_ctl_addr[15:0];
+
+    assign ram_addr_a10_o   = cpu_be_o ? cpu_addr_i[10] : cpu_addr_o[10];
+    assign ram_addr_a11_o   = cpu_be_o ? cpu_addr_i[11] : cpu_addr_o[11];
+    assign ram_addr_a15_o   = cpu_be_o ? cpu_addr_i[15] : cpu_addr_o[15];
+    assign ram_addr_a16_o   = cpu_be_o ? 1'b0 : ram_ctl_addr[16];
+
+    // synthesis off
+    always_ff @(posedge sys_clock_i or negedge sys_clock_i) begin
+        assert(!cpu_be_o || !ram_wb_stall) else $fatal(1, "WB<->RAM bridge must be stalled when CPU is driving bus");
+        assert(!cpu_be_o || !ram_ctl_oe) else $fatal(1, "WB<->RAM bridge must not assert OE when CPU is driving bus");
+        assert(!cpu_be_o || !ram_ctl_we) else $fatal(1, "WB<->RAM bridge must not assert WE when CPU is driving bus");
+        assert(!cpu_be_o || !ram_wb_ack) else $fatal(1, "WB<->RAM bridge must not assert ACK when CPU is driving bus");
+        assert(!io_oe_o  ||  cpu_be_o) else $fatal(1, "IO must not be active unless CPU is driving bus");
+    end
+    // synthesis on
 
     vsync vsync (
         .cpu_clock_i(cpu_clock_o),
