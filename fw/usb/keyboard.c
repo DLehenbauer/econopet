2f9c5799fd540e314dc3f9c0aa5cf51a133aa2d1
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2024-11-05 14:48:24 +0000

    fw: handle shift/unshift (wip)

--- fw/usb/keyboard.c
+++ fw/usb/keyboard.c
@@ -21,12 +21,13 @@ typedef struct __attribute__((packed)) {
 
     // Second byte contains flags
     unsigned int reserved: 6;
-    unsigned int deshift: 1;    // 0 = Normal, 1 = Implicitly remove shift
+    unsigned int unshift: 1;    // 0 = Normal, 1 = Implicitly remove shift
     unsigned int shift: 1;      // 0 = Normal, 1 = Implicitly add shift
 } KeyInfo;
 
 typedef struct {
     uint8_t key;
+    uint8_t modifiers;
     bool pressed;
 } KeyEvent;
 
@@ -46,30 +47,32 @@ static void enqueue_key_event(KeyEvent keyEvent) {
     }
 }
 
-static void enqueue_key_up(uint8_t keycode) {
+static void enqueue_key_up(uint8_t keycode, uint8_t modifiers) {
     KeyEvent keyEvent = {
         .key = keycode,
         .pressed = false,
+        .modifiers = modifiers,
     };
     enqueue_key_event(keyEvent);
 }
 
-static void enqueue_key_down(uint8_t keycode) {
+static void enqueue_key_down(uint8_t keycode, uint8_t modifiers) {
     KeyEvent keyEvent = {
         .key = keycode,
         .pressed = true,
+        .modifiers = modifiers,
     };
     enqueue_key_event(keyEvent);
 }
 
-static bool dequeue_key_event(KeyEvent* keyEvent) {
-    if (key_buffer_head == key_buffer_tail) {
-        return false;
-    } else {
-        *keyEvent = key_buffer[key_buffer_tail];
-        key_buffer_tail = (key_buffer_tail + 1) & KEY_BUFFER_CAPACITY_MASK;
-        return true;
-    }
+static bool peek_key_event(KeyEvent* keyEvent) {
+    *keyEvent = key_buffer[key_buffer_tail];
+    return key_buffer_head != key_buffer_tail;
+}
+
+static void dequeue_key_event() {
+    assert(key_buffer_head != key_buffer_tail);
+    key_buffer_tail = (key_buffer_tail + 1) & KEY_BUFFER_CAPACITY_MASK;
 }
 
 static const uint8_t keymap_raw[] = {
@@ -115,7 +118,7 @@ void key_down(uint8_t keycode) {
 
     if (key_matrix[col] & rowMask) {
         key_matrix[col] &= ~rowMask;
-        printf("USB: Key down: %d=(%d,%d)\n", keycode, ffs(rowMask) - 1, col);
+        printf("USB: Key down: %d=(%d,%d)\n", keycode, row, col);
     }
 }
 
@@ -133,54 +136,104 @@ void key_up(uint8_t keycode) {
 
     if (key_matrix[col] & ~rowMask) {
         key_matrix[col] |= rowMask;
-        printf("USB: Key up: %d=(%d,%d)\n", keycode, ffs(rowMask) - 1, col);
+        printf("USB: Key up: %d=(%d,%d)\n", keycode, row, col);
     }
 }
 
+KeyInfo get_key_info(KeyEvent keyEvent) {
+    KeyInfo keyInfo = s_keymap[keyEvent.key];
+
+    // If the key is pressed, apply shift/unshift modifiers.  We do not adjust
+    // modifiers when the key is released since we want to restore the original
+    // keyboard state before processing more keys.
+    if (keyEvent.pressed) {
+        if (keyInfo.shift) {
+            keyEvent.modifiers |= HID_KEY_SHIFT_LEFT;
+        }
+
+        if (keyInfo.unshift) {
+            keyEvent.modifiers &= ~HID_KEY_SHIFT_LEFT;
+            keyEvent.modifiers &= ~HID_KEY_SHIFT_RIGHT;
+        }
+    }
+
+    return keyInfo;
+}
+
 void dispatch_key_events() {
+    static uint8_t previous_modifiers = 0;
+
+    static const char* const modifiers[] = {
+        "CONTROL_LEFT",
+        "SHIFT_LEFT",
+        "ALT_LEFT",
+        "GUI_LEFT",
+        "CONTROL_RIGHT",
+        "SHIFT_RIGHT",
+        "ALT_RIGHT",
+        "GUI_RIGHT",
+    };
+    
     KeyEvent keyEvent;
-    while (dequeue_key_event(&keyEvent)) {
+
+    if (!peek_key_event(&keyEvent)) {
+        return;
+    }
+
+    KeyInfo keyInfo = get_key_info(keyEvent);
+
+    uint8_t current_modifiers = keyEvent.modifiers;
+    if (current_modifiers != previous_modifiers) {
+        for (uint8_t i = 0; i < 8; i++) {
+            uint8_t current_modifier = current_modifiers & 0x01;
+            uint8_t previous_modifier = previous_modifiers & 0x01;
+
+            if (current_modifier) {
+                if (!previous_modifier) {
+                    printf("USB: Modifier down: %s\n", modifiers[i]);
+                    key_down(HID_KEY_CONTROL_LEFT + i);
+                }
+            } else if (previous_modifier) {
+                printf("USB: Modifier up: %s\n", modifiers[i]);
+                key_up(HID_KEY_CONTROL_LEFT + i);
+            }
+
+            current_modifiers >>= 1;
+            previous_modifiers >>= 1;
+        }
+
+        previous_modifiers = current_modifiers;
+    }
+
+    do {
+        KeyInfo keyInfo = get_key_info(keyEvent);
+
         if (keyEvent.pressed) {
             key_down(keyEvent.key);
         } else {
             key_up(keyEvent.key);
         }
-    }
+
+        dequeue_key_event();
+    } while (peek_key_event(&keyEvent) && keyEvent.modifiers == current_modifiers);
 }
 
 void process_kbd_report(hid_keyboard_report_t const* report) {
     static hid_keyboard_report_t prev_report = {0, 0, {0}};
 
-    uint8_t current_modifiers = report->modifier;
-    uint8_t previous_modifiers = prev_report.modifier;
-
-    for (uint8_t i = 0; i < 8; i++) {
-        uint8_t current_modifier = current_modifiers & 0x01;
-        uint8_t previous_modifier = previous_modifiers & 0x01;
-
-        if (current_modifier) {
-            if (!previous_modifier) {
-                enqueue_key_down(HID_KEY_CONTROL_LEFT + i);
-            }
-        } else if (previous_modifier) {
-            enqueue_key_up(HID_KEY_CONTROL_LEFT + i);
-        }
-
-        current_modifiers >>= 1;
-        previous_modifiers >>= 1;
-    }
+    uint8_t modifiers = report->modifier;
 
     for (uint8_t i = 0; i < 6; i++) {
         uint8_t keycode = prev_report.keycode[i];
         if (keycode && !find_key_in_report(report, keycode)) {
-            enqueue_key_up(keycode);
+            enqueue_key_up(keycode, modifiers);
         }
     }
 
     for (uint8_t i = 0; i < 6; i++) {
         uint8_t keycode = report->keycode[i];
         if (keycode && !find_key_in_report(&prev_report, keycode)) {
-            enqueue_key_down(keycode);
+            enqueue_key_down(keycode, modifiers);
         }
     }
 
