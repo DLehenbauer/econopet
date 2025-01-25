229af3f6c54466c8ef4c6032fbded77560adefc1
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2025-01-25 11:30:08 -0800

    fw: fix paths in roms.sh

--- fw/src/roms/roms.sh
+++ fw/src/roms/roms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for r in ../../build/roms/bin/roms/*
+for r in ../../../build/roms/bin/roms/*
 do
     f="$(basename -- $r .bin).h"
     cat $r | xxd -i > $f
@@ -9,4 +9,4 @@ done
 cp edit-4-80-b-60Hz.901474-03.h edit-4-80-n-60Hz.901474-03-hack.h
 patch edit-4-80-n-60Hz.901474-03-hack.h edit-4-80-n-60Hz.901474-03-hack.diff
 
-cat ../../rom/rom.bin | xxd -i > menu.h
+cat ../../../rom/rom.bin | xxd -i > menu.h
