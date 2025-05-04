4b36dcee710883c59cccd9a90ecef42ee7eb6bdd
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2025-05-04 19:42:36 +0000

    fw: add missing files

---
+++ fw/src/roms/roms.h
@@ -0,0 +1,23 @@
+/**
+ * PET Clone - Open hardware implementation of the Commodore PET
+ * by Daniel Lehenbauer and contributors.
+ * 
+ * https://github.com/DLehenbauer/commodore-pet-clone
+ *
+ * To the extent possible under law, I, Daniel Lehenbauer, have waived all
+ * copyright and related or neighboring rights to this project. This work is
+ * published from the United States.
+ *
+ * @copyright CC0 http://creativecommons.org/publicdomain/zero/1.0/
+ * @author Daniel Lehenbauer <DLehenbauer@users.noreply.github.com> and contributors
+ */
+
+#pragma once
+
+#include "../pch.h"
+
+extern const uint8_t rom_chars_8800[0x800];
+extern const uint8_t* const p_video_font_000;
+extern const uint8_t* const p_video_font_400;
+
+extern const uint8_t rom_menu_ff00[0x100];
