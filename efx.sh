bdde9e4ee5a8492bf364955df3308dd7d4d4a6fc
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2025-06-22 17:32:02 +0000

    gw: add to CMake build

--- efx.sh
+++
@@ -1,4 +0,0 @@
-source /opt/efinity/2025.1/bin/setup.sh
-pushd gw/EconoPET
-$EFINITY_HOME/scripts/efx_run.py EconoPET.xml "$@"
-popd
---
+++ gw/efx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source $EFINITY_PATH/bin/setup.sh
+pushd EconoPET
+$EFINITY_HOME/scripts/efx_run.py EconoPET.xml "$@"
+cat outflow/EconoPET.warn.log
+cat outflow/EconoPET.err.log
+popd
