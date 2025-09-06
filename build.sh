3e1a464fb3c3da568b18b51400e4ecbf743a6cd9
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2025-09-06 15:27:25 +0000

    tidy build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-
-# Exit immediately if a command exits with a non-zero status.
 set -e
-
-# Print each command before executing it.
-set -x
 
 # Save current directory and change to the directory containing this script
 ORIG_PWD="$(pwd)"
