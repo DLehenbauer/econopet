db9242a04f6466fcc043c895956678bd13bad449
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2025-09-07 14:16:53 +0000

    gw: tidy efx.sh

--- gw/efx.sh
+++ gw/efx.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -e
 
 source $EFINITY_HOME/bin/setup.sh
 
-pushd $(dirname "$0")/EconoPET && \
-    python3 -u $EFINITY_HOME/scripts/efx_run.py EconoPET.xml "$@" && \
-    popd
+# Ensure we restore the original directory on exit
+ORIG_PWD="$(pwd)"
+trap 'cd "$ORIG_PWD"' EXIT
+
+# Change to the './EconoPET' directory relative to this script's location
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR/EconoPET"
+
+python3 -u $EFINITY_HOME/scripts/efx_run.py EconoPET.xml "$@"
